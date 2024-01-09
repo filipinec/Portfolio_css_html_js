645a0bede75d565d0a17af882db6c750244ec424
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-09 17:35:07 +0100

    update

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function toggleMenu() {
  menu.classList.toggle("open");
  icon.classList.toggle("open");
 }
+
+//Modal window
+const btnsOpenModal = document.querySelectorAll(".btn-show-modal");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+
+// Function for Open a Modal window
+const openModal = function () {
+ modal.classList.remove("hidden");
+ overlay.classList.remove("hidden");
+};
+// Function for Close a Modal window
+const closeModal = function (e) {
+ e.preventDefault();
+ modal.classList.add("hidden");
+ overlay.classList.add("hidden");
+};
+
+// add event listener for 2 btn for Open
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+// add event listener for Close
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+ if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+  closeModal();
+ }
+});
