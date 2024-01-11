8be4409e911da85d65469ca7fb3f19a494f08598
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-11 11:18:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 // add event listener for Close
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
-
 document.addEventListener("keydown", function (e) {
  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
   closeModal();
