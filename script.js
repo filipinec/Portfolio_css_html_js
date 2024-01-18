b073dcb1b2d16446ef0dc4e982df7bbe69917aaf
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-18 10:25:38 +0100

    update

--- script.js
+++ script.js
@@ -35,6 +35,32 @@ document.addEventListener("keydown", function (e) {
  }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+ const clicked = e.target.closest(".operations__tab");
+ // Guard clause
+ if (!clicked) return;
+
+ //Active Tab
+ tabs.forEach(function (el) {
+  el.classList.remove("operations__tab--active");
+ });
+ tabsContent.forEach(function (el) {
+  el.classList.remove("operations__content--active");
+ });
+
+ clicked.classList.add("operations__tab--active");
+
+ // Active Content Area
+ document
+  .querySelector(`.operations__content--${clicked.getAttribute("data-tab")}`)
+  .classList.add("operations__content--active");
+});
+
 // /////////////////////////////////
 
 // // Divide duplicates and non-duplicates number
