b807a04865b3bfbdc27f45ae81968f62369c7d99
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-21 16:00:55 +0100

    Update Progress bar

--- script.js
+++ script.js
@@ -59,6 +59,30 @@ tabsContainer.addEventListener("click", function (e) {
   .querySelector(`.operations__content--${clicked.getAttribute("data-tab")}`)
   .classList.add("operations__content--active");
 });
+
+// Progress Bar
+const progressBar = function (skillId, numOfCircle, num, timeForLoad) {
+ let numElement = document.querySelector(`#${skillId} .inner`);
+ let root = document.documentElement;
+ let counter = 0;
+ let intervalId;
+
+ root.style.setProperty("--changeHTML", numOfCircle + 219);
+ root.style.setProperty("--changeCSS", numOfCircle + 106);
+
+ intervalId = setInterval(() => {
+  if (counter == num) {
+   clearInterval(intervalId);
+  } else {
+   counter = counter + 1;
+   numElement.innerHTML = counter + "%";
+  }
+ }, timeForLoad);
+};
+
+progressBar("skill-1", 0, 50, 40);
+progressBar("skill-2", 0, 75, 27);
+
 // /////////////////////////////////
 
 // // Divide duplicates and non-duplicates number
